db9b76619d7cfea0a11f5a0a052bf89d55faa0d6
Nickjohn18 <81334326+Nickjohn18@users.noreply.github.com>
2021-07-10 20:36:02 -0400

    save button not comming up

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
@@ -21,13 +17,24 @@ app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
-app.get("/api/notes:id", (req, res) => {
-  const notesSaved = fs.readFileSync("./db/db.json", "utf8");
-  res.json(notesSaved(req.params.id));
+app.get("/api/notes", (req, res) => {
+  const notesSaved = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(notesSaved[Number(req.params.id)]);
 });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  const notesSaved = fs.readFileSync("./db/db.json", "utf8");
+  const note = req.body;
+  note.id = notesSaved.length.toString();
+  notesSaved.push(note);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(notesSaved));
+  console.log(`Note Saved! New note: ${note}`);
+  res.json(notesSaved);
 });
 
 app.listen(PORT, () => {
