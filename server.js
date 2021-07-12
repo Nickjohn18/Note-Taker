4faba311aeb167ee39297492d668f303e9d30b4c
Nickjohn18 <81334326+Nickjohn18@users.noreply.github.com>
2021-07-11 20:39:24 -0400

    added route to index

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
